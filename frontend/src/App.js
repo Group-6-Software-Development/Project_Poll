3be12f789f5f1f0ea008625d1a84d1017eea728e
MakeLord95 <juhani.markus@gmail.com>
2024-02-15 00:42:05 +0200

    Navbar WIP

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,9 @@
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar isLoggedIn={false} />
       <header className="App-header">
         <p>
           Edit <code>src/App.js</code> and save to reload.
