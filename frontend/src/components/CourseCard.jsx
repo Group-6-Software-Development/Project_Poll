9d7e9b4377d97480c219911fc21df36046712a98
Kim Löfgren <kim.lofgren@metropolia.fi>
2024-02-26 21:57:53 +0200

    course card editing

--- frontend/src/components/CourseCard.jsx
+++ frontend/src/components/CourseCard.jsx
@@ -1,23 +1,78 @@
 import classPhoto from "../images/classroomPhoto.png";
 import "./styles/CourseCard.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPen, faCheck } from "@fortawesome/free-solid-svg-icons";
+
+import { useState } from "react";
+
+const editButton = (
+  <FontAwesomeIcon icon={faPen} style={{ color: "#ff5000" }} size="xl" />
+);
+
+const saveButton = (
+  <FontAwesomeIcon icon={faCheck} style={{ color: "#5cb85c" }} size="xl" />
+);
 
 const CourseCard = () => {
+  const handleEditClick = () => {
+    setIsEditing(!isEditing);
+  };
+  const handleSaveClick = () => {
+    // #TODO: validate changes and save to db
+    setIsEditing(!isEditing);
+  };
+
+  const [isEditing, setIsEditing] = useState(false);
+  const [courseId, setCourseId] = useState("CourseID");
+  const [courseName, setCourseName] = useState("Course Name");
+  const [courseDate, setCourseDate] = useState("Date");
+
   return (
     <div className="course-card">
       <img src={classPhoto} alt="classroom"></img>
+      {isEditing ? (
+        <button onClick={handleSaveClick}>{saveButton}</button>
+      ) : (
+        <button onClick={handleEditClick}>{editButton}</button>
+      )}
+
       <div className="course-info">
         <div className="course-id">
-          <p>TX00EY27-T3ST</p>
+          {isEditing ? (
+            <input
+              type="text"
+              value={courseId}
+              onChange={(e) => setCourseId(e.target.value)}
+            />
+          ) : (
+            <p>{courseId}</p>
+          )}
         </div>
         <div className="course-name">
-          <h4>Course Name</h4>
+          {isEditing ? (
+            <input
+              type="text"
+              value={courseName}
+              onChange={(e) => setCourseName(e.target.value)}
+            />
+          ) : (
+            <h4>{courseName}</h4>
+          )}
         </div>
         <div className="course-date">
-          <p>15.2.2024-7.3.2024</p>
+          {isEditing ? (
+            <input
+              type="text"
+              value={courseDate}
+              onChange={(e) => setCourseDate(e.target.value)}
+            />
+          ) : (
+            <p>{courseDate}</p>
+          )}
         </div>
       </div>
     </div>
   );
 };
-// TODO: there still need changes to make loading of unique data
+
 export default CourseCard;
