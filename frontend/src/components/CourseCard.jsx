45e0a8cee9ac864a82bf1fb7de2dcc175a10ff47
samwise50000 <sami.mattila3@metropolia.fi>
2024-02-29 14:50:26 +0200

    Frontend tests done for now

--- frontend/src/components/CourseCard.jsx
+++ frontend/src/components/CourseCard.jsx
@@ -1,7 +1,7 @@
+import { faCheck, faPen } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import classPhoto from "../images/classroomPhoto.png";
 import "./styles/CourseCard.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPen, faCheck } from "@fortawesome/free-solid-svg-icons";
 
 import { useState } from "react";
 
@@ -29,7 +29,7 @@ const CourseCard = () => {
 
   return (
     <div className="course-card">
-      <img src={classPhoto} alt="classroom"></img>
+      <img src={classPhoto} alt="classroom" />
       {isEditing ? (
         <button onClick={handleSaveClick}>{saveButton}</button>
       ) : (
@@ -38,8 +38,10 @@ const CourseCard = () => {
 
       <div className="course-info">
         <div className="course-id">
+          <label htmlFor="courseId">Course ID</label>
           {isEditing ? (
             <input
+              id="courseId"
               type="text"
               value={courseId}
               onChange={(e) => setCourseId(e.target.value)}
@@ -49,8 +51,10 @@ const CourseCard = () => {
           )}
         </div>
         <div className="course-name">
+          <label htmlFor="courseName">Course Name</label>
           {isEditing ? (
             <input
+              id="courseName"
               type="text"
               value={courseName}
               onChange={(e) => setCourseName(e.target.value)}
@@ -60,8 +64,10 @@ const CourseCard = () => {
           )}
         </div>
         <div className="course-date">
+          <label htmlFor="courseDate">Date</label>
           {isEditing ? (
             <input
+              id="courseDate"
               type="text"
               value={courseDate}
               onChange={(e) => setCourseDate(e.target.value)}
