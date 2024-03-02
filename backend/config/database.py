399e54ddbb57129aa5b07e2a2321ff13388e6ba6
MakeLord95 <juhani.markus@gmail.com>
2024-03-02 06:15:06 +0200

    Refactoring

--- backend/config/database.py
+++ backend/config/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-from config.env_config import DATABASE_URL
+from config.env_config import DATABASE_URL, DATABASE_NAME
 
-engine = create_engine(DATABASE_URL, pool_pre_ping=True)
+engine = create_engine(DATABASE_URL + DATABASE_NAME, pool_pre_ping=True)
 Session = sessionmaker(bind=engine)
--- backend/config/env_config.py
+++ backend/config/env_config.py
@@ -5,7 +5,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DATABASE_URL = getenv('DATABASE_URL')
+DATABASE_NAME = getenv('DATABASE_NAME')
+
 JWT_SECRET = getenv('JWT_SECRET', '64-bytes-of-secret')
 SALT_ROUNDS = getenv('SALT_ROUNDS', 12)
+
 PORT = getenv('PORT', 5000)
 JWT_TOKEN_TIME_TO_LIVE = getenv('JWT_TOKEN_TIME_TO_LIVE', 3600)
