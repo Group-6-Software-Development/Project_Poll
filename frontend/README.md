973c3a660c2aea415d98a8bc71cae742277e2d59
MakeLord95 <juhani.markus@gmail.com>
2024-03-14 02:09:00 +0200

    REAME updates Fix bug in register

--- frontend/README.md
+++ frontend/README.md
@@ -1,6 +1,20 @@
 # Frontend
 
-How to run the frontend:
+## Configuration
+
+The following files have something that you need to configure to use the frontend:
+
+- [CourseCard](../frontend/src/components/CourseCard.jsx) Lines 54 & 134
+- [LectureCard](../frontend/src/components/LectureCard.jsx) Lines 36 & 75
+- [useLogin](../frontend/src/hooks/useLogin.js) Line 6
+- [useRegister](../frontend/src/hooks/useRegister.js) Line 6
+- [LecturePreview](../frontend/src/pages/LecturePreview.js) Lines 15 & 51
+- [LectureReview](../frontend/src/pages/LectureReview.js) Lines 46 & 80
+- [UserPage](../frontend/src/pages/UserPage.js) Lines 14 & 46
+- [LinkPage](../frontend/src/pages/LinkPage.js) Line 6
+- [ReviewPage](../frontend/src/pages/ReviewPage.js) Line 53
+
+## How to run the frontend
 
 1. Install Node.js
 2. Run `npm install` in the frontend directory
--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -6,11 +6,12 @@ import "./styles/Register.css";
 const Register = ({ setIsAuthenticated }) => {
   const register = useRegister({ setIsAuthenticated });
 
+  const [error, setError] = useState("");
   const firstName = useField("firstName");
   const lastName = useField("lastName");
   const email = useField("email");
   const password = useField("password");
-  const passwordAgain = useField("passwordAgain");
+  const passwordAgain = useField("password");
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -27,20 +28,55 @@ const Register = ({ setIsAuthenticated }) => {
   return (
     <div className="register-page">
       <form className="register-form" onSubmit={handleSubmit}>
-        <label className="first-name-message" htmlFor="firstName">First Name:</label>
-        <input {...firstName} className="first-name-input" required data-testid="first-name-input" />
+        <label className="first-name-message" htmlFor="firstName">
+          First Name:
+        </label>
+        <input
+          {...firstName}
+          className="first-name-input"
+          required
+          data-testid="first-name-input"
+        />
 
-        <label className="last-name-message" htmlFor="lastName">Last Name:</label>
-        <input {...lastName} className="last-name-input" required data-testid="last-name-input" />
+        <label className="last-name-message" htmlFor="lastName">
+          Last Name:
+        </label>
+        <input
+          {...lastName}
+          className="last-name-input"
+          required
+          data-testid="last-name-input"
+        />
 
-        <label className="email-message" htmlFor="email">Email:</label>
-        <input {...email} className="email-input" required data-testid="email-input" />
+        <label className="email-message" htmlFor="email">
+          Email:
+        </label>
+        <input
+          {...email}
+          className="email-input"
+          required
+          data-testid="email-input"
+        />
 
-        <label className="password-message" htmlFor="password">Password:</label>
-        <input {...password} className="password-input" required data-testid="password-input"/>
+        <label className="password-message" htmlFor="password">
+          Password:
+        </label>
+        <input
+          {...password}
+          className="password-input"
+          required
+          data-testid="password-input"
+        />
 
-        <label className="password-again-message" htmlFor="passwordAgain">Password again:</label>
-        <input {...passwordAgain} className="password-again-input" required data-testid="password-again-input"/>
+        <label className="password-again-message" htmlFor="passwordAgain">
+          Confirm password:
+        </label>
+        <input
+          {...passwordAgain}
+          className="password-again-input"
+          required
+          data-testid="password-again-input"
+        />
 
         {error && (
           <p
@@ -50,7 +86,11 @@ const Register = ({ setIsAuthenticated }) => {
           </p>
         )}
 
-        <button className="register-button" type="submit"  data-testid="register-form">
+        <button
+          className="register-button"
+          type="submit"
+          data-testid="register-form"
+        >
           Register
         </button>
       </form>
