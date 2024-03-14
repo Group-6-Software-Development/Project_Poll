a0a5222125d9af80e015c8e08dba9b644fac6bb8
Sami <sami.mattila3@metropolia.fi>
2024-03-15 01:59:34 +0200

    some nasty changes to backend so we got a bit forward with establishing the database with docker..

--- backend/models/user_model.py
+++ backend/models/user_model.py
@@ -1,18 +1,16 @@
 import uuid
-
 import bcrypt
 from sqlalchemy import Column, String
-from sqlalchemy.dialects.postgresql import UUID
-
-from config.base import Base
+from sqlalchemy.ext.declarative import declarative_base
 from config.database import Session
 from config.env_config import SALT_ROUNDS
 
+Base = declarative_base()
 
 class UserModel(Base):
     __tablename__ = 'users'
 
-    uuid = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
+    uuid = Column(String(36), primary_key=True, default=str(uuid.uuid4()), unique=True, nullable=False)
     email = Column(String(50), unique=True, nullable=False)
     first_name = Column(String(50), nullable=False)
     last_name = Column(String(50), nullable=False)
@@ -20,13 +18,12 @@ class UserModel(Base):
 
 
 def signup(first_name, last_name, email, password):
-    user = UserModel(first_name=first_name, last_name=last_name, email=email,
-                     password=bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt(rounds=int(SALT_ROUNDS))).decode(
-                         'utf-8'))
+    user = UserModel(uuid=str(uuid.uuid4()), first_name=first_name, last_name=last_name, email=email,
+                     password=bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt(rounds=int(SALT_ROUNDS))).decode('utf-8'))
     session = Session()
     session.add(user)
     session.commit()
-    user_data = {'uuid': str(user.uuid), 'first_name': user.first_name, 'last_name': user.last_name,
+    user_data = {'uuid': user.uuid, 'first_name': user.first_name, 'last_name': user.last_name,
                  'email': user.email}
     session.close()
     return user_data
@@ -35,7 +32,7 @@ def signup(first_name, last_name, email, password):
 def login(email, password):
     session = Session()
     user = session.query(UserModel).filter_by(email=email).first()
-    user_data = {'uuid': str(user.uuid), 'first_name': user.first_name, 'last_name': user.last_name,
+    user_data = {'uuid': user.uuid, 'first_name': user.first_name, 'last_name': user.last_name,
                  'email': user.email} if user else None
     session.close()
     if user and bcrypt.checkpw(password.encode('utf-8'), user.password.encode('utf-8')):
@@ -46,13 +43,13 @@ def login(email, password):
 
 def update(user_uuid, first_name, last_name, email, password):
     session = Session()
-    user = session.query(UserModel).filter_by(uuid=uuid.UUID(user_uuid)).first()
+    user = session.query(UserModel).filter_by(uuid=user_uuid).first()
     user.email = email
     user.first_name = first_name
     user.last_name = last_name
     user.password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt(rounds=int(SALT_ROUNDS))).decode('utf-8')
     session.commit()
-    user_data = {'uuid': str(user.uuid), 'first_name': user.first_name, 'last_name': user.last_name,
+    user_data = {'uuid': user.uuid, 'first_name': user.first_name, 'last_name': user.last_name,
                  'email': user.email}
     session.close()
     return user_data
@@ -60,8 +57,8 @@ def update(user_uuid, first_name, last_name, email, password):
 
 def find_user_by_uuid(user_uuid):
     session = Session()
-    user = session.query(UserModel).filter_by(uuid=uuid.UUID(user_uuid)).first()
-    user_data = {'uuid': str(user.uuid), 'first_name': user.first_name, 'last_name': user.last_name,
+    user = session.query(UserModel).filter_by(uuid=user_uuid).first()
+    user_data = {'uuid': user.uuid, 'first_name': user.first_name, 'last_name': user.last_name,
                  'email': user.email}
     session.close()
     return user_data
@@ -69,7 +66,7 @@ def find_user_by_uuid(user_uuid):
 
 def delete(user_uuid):
     session = Session()
-    user = session.query(UserModel).filter_by(uuid=uuid.UUID(user_uuid)).first()
+    user = session.query(UserModel).filter_by(uuid=user_uuid).first()
     session.delete(user)
     session.commit()
     session.close()
